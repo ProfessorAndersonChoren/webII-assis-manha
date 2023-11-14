aeccf1f11471fe487170b4d9a0a0142fefadb307
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2023-11-14 10:48:08 -0300

    Sistema de chamados - Inclusão dos métodos assessores e construtor

--- Sistema de chamados/src/Model/Call.php
+++ Sistema de chamados/src/Model/Call.php
@@ -4,10 +4,33 @@ namespace QI\SistemaDeChamados\Model;
 
 class Call{
     private $id;
-    private $user_name;
-    private $user_email;
+    private $user;
     private $equipment;
     private $classification;
     private $description;
-    private $note;
+    private $notes;
+
+    /**
+     * This method create a new Call object
+     * @param User $user
+     * @param Equipment $equipment
+     * @param string $classification
+     * @param string $description
+     */
+    public function __construct($user,$equipment,$classification,$description){
+        $this->user = $user;
+        $this->equipment = $equipment;
+        $this->classification = $classification;
+        $this->description = $description;
+    }
+
+    public function __get($attribute)
+    {
+        return $this->$attribute;
+    }
+
+    public function __set($attribute, $value)
+    {
+        $this->$attribute = $value;
+    }
 }
---
+++ Sistema de chamados/src/Model/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace QI\SistemaDeChamados\Model;
+
+class User{
+    private $id;
+    private $name;
+    private $email;
+    private $password;
+
+    /**
+     * This method create a new User object
+     * @param string $email This is email of user
+     */
+    public function __construct($email){
+        $this->email = $email;
+    }
+
+    public function __get($attribute)
+    {
+        return $this->$attribute;
+    }
+
+    public function __set($attribute, $value){
+        $this->$attribute = $value;
+    }
+}
