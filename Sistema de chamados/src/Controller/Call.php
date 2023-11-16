5d253dcd86a9b2d003299cea6c4f3de0cbc8923a
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2023-11-16 09:35:06 -0300

    Sistema de chamados - Controlador de chamados

---
+++ Sistema de chamados/src/Controller/Call.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace QI\SistemaDeChamados\Controller;
+
+use QI\SistemaDeChamados\Model\Call;
+use QI\SistemaDeChamados\Model\Equipment;
+use QI\SistemaDeChamados\Model\User;
+
+require_once dirname(dirname(__DIR__)) . "/vendor/autoload.php";
+
+session_start();
+switch ($_GET["operation"]) {
+    case "insert":
+        insert();
+        break;
+    default:
+        $_SESSION["msg_warning"] = "Operação inválida!!!";
+        header("location:../View/message.php");
+        exit;
+}
+
+function insert()
+{
+    if (empty($_POST)) {
+        $_SESSION["msg_warning"] = "Ops, houve um erro inesperado!!!";
+        header("location:../View/message.php");
+        exit;
+    }
+
+    $user = new User($_POST["user_email"]);
+    $user->name = $_POST["user_name"];
+    $equipment = new Equipment(
+        $_POST["pc_number"],
+        intval($_POST["floor"]),
+        intval($_POST["room"]),
+    );
+    $call = new Call($user, $equipment, $_POST["classification"], $_POST["description"]);
+
+    if (!empty($_POST["notes"])) {
+        $call->notes = $_POST["notes"];
+    }
+
+    // TODO Validar os dados vindos do formulário
+}
